eb9e82e520fbfb516fc4219b97a7e654b6040a06
agnes <agnes@agnesdeMacBook-Pro.local>
2020-10-13 15:50:37 +0800

    update stackbyarray

--- stackByArray.py
+++ stackByArray.py
@@ -2,7 +2,7 @@
 
 class Stack(object):
     # 初始化栈
-    def __init__(self,x):
+    def __init__(self):
         self.items=[]
 
     # 判断是否为空，为空返回布尔值
@@ -22,5 +22,21 @@ class Stack(object):
         return self.items.append(item)
 
     # 出栈
-    def pop(self, item):
+    def pop(self):
         return self.items.pop()
+
+
+if __name__ == "__main__":
+    my_stack = Stack()          # 初始化实例
+    my_stack.push('1')
+    my_stack.push('2')
+    print("1:",my_stack.size())
+    print("2:",my_stack.peek())
+    print("3:",my_stack.pop())
+    print("4:",my_stack.peek())
+    print("5:",my_stack.size())
+    print("6:",my_stack.pop())
+    print("7:",my_stack.size())
+    print("8:",my_stack.is_empty())
+
+
